4a50e513e363a7c495efeb41d1915f27c67cbd52
Luc Didry <luc@didry.org>
2023-04-13 09:05:34 +0200

    🚨 — Lint scripts

--- update-motd.d/sysinfo.py
+++ update-motd.d/sysinfo.py
@@ -1,96 +1,111 @@
-#!/usr/bin/python
-#
-# landscape-sysinfo-mini.py -- a trivial re-implementation of the 
-# sysinfo printout shown on debian at boot time. No twisted, no reactor, just /proc & utmp
-#
-# (C) 2014 jw@owncloud.com
-#
-# inspired by ubuntu 14.10 /etc/update-motd.d/50-landscape-sysinfo
-# Requires: python-utmp 
-# for counting users.
-#
-# 2014-09-07 V1.0 jw, ad hoc writeup, feature-complete. Probably buggy?
-# 2014-10-08 V1.1 jw, survive without swap
-# 2014-10-13 V1.2 jw, survive without network
+#!/usr/bin/python3
+"""
+landscape-sysinfo-mini.py -- a trivial re-implementation of the
+sysinfo printout shown on debian at boot time. No twisted, no reactor, just /proc & utmp
 
-# Modified by Luc Didry in 2016
-# Get the original version at https://github.com/jnweiger/landscape-sysinfo-mini
+(C) 2014 jw@owncloud.com
 
-import sys,os,time,posix,glob,utmp
-from UTMPCONST import *
+inspired by ubuntu 14.10 /etc/update-motd.d/50-landscape-sysinfo
+Requires: python3-utmp 
+for counting users.
 
-_version_ = '1.2'
+2014-09-07 V1.0 jw, ad hoc writeup, feature-complete. Probably buggy?
+2014-10-08 V1.1 jw, survive without swap
+2014-10-13 V1.2 jw, survive without network
+
+Modified by Luc Didry in 2016 and 2023
+Get the original version at https://github.com/jnweiger/landscape-sysinfo-mini
+"""
+
+
+import sys
+import os
+import time
+import glob
+import utmp  # type: ignore
+
+VERSION = '1.3'
 
 def utmp_count():
-  u = utmp.UtmpRecord()
-  users = 0
-  for i in u:
-    if i.ut_type == utmp.USER_PROCESS: users += 1
-  return users
+    """ Count user processes
+    """
+    u_r = utmp.UtmpRecord()
+    l_users = 0
+    for i in u_r:
+        if i.ut_type == utmp.USER_PROCESS:
+            l_users += 1
+    return l_users
 
 def proc_meminfo():
-  items = {}
-  for l in open('/proc/meminfo').readlines():
-    a = l.split()
-    items[a[0]] = int(a[1])
-  # print items['MemTotal:'], items['MemFree:'], items['SwapTotal:'], items['SwapFree:']
-  return items
+    """ Get memory usage informations
+    """
+    items = {}
+    for line in open('/proc/meminfo', encoding="ASCII").readlines():  # pylint: disable-msg=R1732
+        array = line.split()
+        items[array[0]] = int(array[1])
+    return items
 
 def proc_mount():
-  items = {}
-  for m in open('/proc/mounts').readlines():
-    a = m.split()
-    if a[0].find('/dev/') == 0:
-      statfs = os.statvfs(a[1])
-      perc = 100-100.*statfs.f_bavail/statfs.f_blocks if statfs.f_blocks != 0 else 100
-      gb = statfs.f_bsize*statfs.f_blocks/1024./1024/1024
-      items[a[1]] = "%.1f%% of %.2fGB" % (perc, gb)
-  return items
+    """ Get disks space usage
+    """
+    items = {}
+    for mount in open('/proc/mounts', encoding="ASCII").readlines():  # pylint: disable-msg=R1732
+        array = mount.split()
+        if array[0].find('/dev/') == 0:
+            l_statfs = os.statvfs(array[1])
+            perc = 100 - 100. * l_statfs.f_bavail / l_statfs.f_blocks \
+                    if l_statfs.f_blocks != 0 else 100
+            g_b = l_statfs.f_bsize * l_statfs.f_blocks / 1024. / 1024 / 1024
+            items[array[1]] = f"{perc:5.1f}% of {g_b:.2f}GB"
+    return items
 
 def inode_proc_mount():
-  items = {}
-  for m in open('/proc/mounts').readlines():
-    a = m.split()
-    if a[0].find('/dev/') == 0:
-      statfs = os.statvfs(a[1])
-      perc = 100-100.*statfs.f_ffree/statfs.f_files if statfs.f_files != 0 else 100
-      iTotal = statfs.f_files
-      items[a[1]] = "%.1f%% of %.2d" % (perc, iTotal)
-  return items
+    """ Get disks inode usage
+    """
+    items = {}
+    for mount in open('/proc/mounts', encoding="ASCII").readlines():  # pylint: disable-msg=R1732
+        array = mount.split()
+        if array[0].find('/dev/') == 0:
+            l_statfs = os.statvfs(array[1])
+            perc = 100 - 100. * l_statfs.f_ffree / l_statfs.f_files \
+                    if l_statfs.f_files != 0 else 100
+            i_total = l_statfs.f_files
+            items[array[1]] = f"{perc:5.1f}% of {i_total}"
+    return items
 
-loadav = float(open("/proc/loadavg").read().split()[1])
+with open("/proc/loadavg", encoding="ASCII") as avg_line:
+    loadav = float(avg_line.read().split()[1])
 processes = len(glob.glob('/proc/[0-9]*'))
 statfs = proc_mount()
-iStatfs = inode_proc_mount()
-users = utmp_count()
+i_statfs = inode_proc_mount()
+USERS = utmp_count()
 meminfo = proc_meminfo()
-memperc = "%d%%" % (100-100.*meminfo['MemAvailable:']/(meminfo['MemTotal:'] or 1))
-swapperc = "%d%%" % (100-100.*meminfo['SwapFree:']/(meminfo['SwapTotal:'] or 1))
+memperc = f"{100 - 100. * meminfo['MemAvailable:'] / (meminfo['MemTotal:'] or 1):.2f}%"
+SWAPPERC = f"{100 - 100. * meminfo['SwapFree:'] / (meminfo['SwapTotal:'] or 1):.2f}%"
 
-if meminfo['SwapTotal:'] == 0: swapperc = '---'
+if meminfo['SwapTotal:'] == 0:
+    SWAPPERC = '---'
 
-print("  System information as of %s\n" % time.asctime())
-print("  System load:  %-5.2f                Processes:           %d" % (loadav, processes))
-print("  Memory usage: %-4s                 Users logged in:     %d" % (memperc, users))
-print("  Swap usage:   %s" % (swapperc))
+print(f"  System information as of {time.asctime()}\n")
+print(f"  System load:  {loadav: <5.2f}                Processes:           {processes}")
+print(f"  Memory usage: {memperc: <4}               Users logged in:     {USERS}")
+print(f"  Swap usage:   {SWAPPERC}")
 
 print("  Disk Usage:")
 for k in sorted(statfs.keys()):
-  print("    Usage of %-24s: %-20s" % (k, statfs[k]))
+    print(f"    Usage of {k: <24}: {statfs[k]: <20}")
 
 print("  Inode Usage:")
-for l in sorted(iStatfs.keys()):
-  print("    Usage of %-24s: %-20s" % (l, iStatfs[l]))
+for l in sorted(i_statfs.keys()):
+    print(f"    Usage of {l: <24}: {i_statfs[l]: <20}")
 
-if users > 0:
+if USERS > 0:
     a = utmp.UtmpRecord()
-
     print("\n  Logged in users:")
-
-    for b in a: # example of using an iterator
-        if b.ut_type == USER_PROCESS:
-            print("  \033[1;31m%-10s\033[m from %-25s at %-20s" % \
-            (b.ut_user, b.ut_host, time.ctime(b.ut_tv[0])))
+    for b in a:
+        if b.ut_type == utmp.USER_PROCESS:
+            print(f"  \033[1;31m{b.ut_user: <10}\033[m from {b.ut_host: <25}"
+                  f" at {time.ctime(b.ut_tv[0]): <20}")
     a.endutent()
 
 sys.exit(0)
